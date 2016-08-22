607e46886fb99578561d4d285c0d8edf0e78e9cc
saurabh <saurabhbaliyan000@gmail.com>
2016-08-22 05:45:33 -0700

    Add files via upload

---
+++ imgur/imgur/spiders/imgur_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+from scrapy.contrib.spiders import Rule, CrawlSpider
+from scrapy.contrib.linkextractors import LinkExtractor
+from imgur.items import ImgurItem
+
+class ImgurSpider(CrawlSpider):
+    
+
+
+    name='imgur'
+    allowed_domains=['facebook.com']
+    start_urls=['https://www.facebook.com/saurabh.baliyan.1']
+    rules=[Rule(LinkExtractor(allow=['/gallery/.*']),'parse_imgur')]
+    
+    def parse_imgur(self,response):
+        
+        image=ImgurItem()
+        
+        image['title']=response.xpath(\
+            "//*[@id='u_0_1w']/div[2]/div[1]/div[5]/div/div/div/a").extract()
+        rel=response.xpath("//*[@id='u_0_2c']").extract()
+        image['image_urls']=['http:'+rel[0]]
+        return image
+            
+            
+        
+            
+	
